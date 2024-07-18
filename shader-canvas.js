ef4303bbb250d271cd1e48c7a76cf166fd7f5488
Giulia Polenta <giuliapolenta@gmail.com>
2024-07-18 08:26:39 +0200

    Add base code

---
+++ shader-canvas.js
@@ -0,0 +1,13 @@
+const shader = () => {
+    const cover = document.querySelector(".cover");
+    const canvas = document.createElement("canvas");
+    const sandbox = new GlslCanvas(canvas);
+
+    cover.appendChild(canvas);
+    sandbox.load(frag);
+    sandbox.setUniform("seed", Math.random());
+}
+
+window.addEventListener("load", () => {
+    shader();
+})
