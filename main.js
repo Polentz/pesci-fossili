99f90bd856aae7316b0f1fb1b2351c627d27bc05
Giulia Polenta <giuliapolenta@gmail.com>
2024-07-18 17:08:33 +0200

    Add content

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// import { animate, stagger, inView } from 'motion';
+import { animate, stagger, inView } from 'motion';
 import {
     DirectionalLight,
     Mesh,
@@ -10,21 +10,21 @@ import {
 } from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// animate(
-//     '.grid-menu',
-//     {
-//         opacity: [0, 1],
-//     },
-//     { duration: 1 },
-// );
+animate(
+    '.grid-menu',
+    {
+        opacity: [0, 1],
+    },
+    { duration: 1 },
+);
 
-// animate(
-//     ".sidebar li",
-//     {
-//         opacity: [0, 1],
-//     },
-//     { duration: 1, delay: stagger(0.35) },
-// );
+animate(
+    ".sidebar li",
+    {
+        opacity: [0, 1],
+    },
+    { duration: 1, delay: stagger(0.35) },
+);
 
 // animate(
 //     ".introduction-container",
@@ -37,8 +37,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // animate('.introduction-container p', { opacity: 0 });
 
-// inView('.introduction-container', (info) => {
-//     animate(info.target.querySelectorAll('p'), { opacity: 1 }, { duration: 1, delay: 1 })
+// inView('.introduction-container p', (info) => {
+//     animate(info.target, { opacity: 1 }, { duration: 1, delay: 0.25 })
 // });
 
 const object = (container, shape) => {
@@ -91,13 +91,6 @@ const object = (container, shape) => {
     window.addEventListener("resize", resize);
 };
 
-// divs.forEach(container => {
-//     const geometry = new TorusKnotGeometry(3, 0.3, 300, 20, 8, 6);
-//     const material = new MeshLambertMaterial({ color: 0xffffff });
-//     const shape = new Mesh(geometry, material);
-//     object(container, shape);
-// });
-
 const createOne = () => {
     const container = document.querySelector("#bennet-content .shape-container");
     const geometry = new TorusKnotGeometry(3, 0.8, 300, 20, 8, 4);
@@ -108,40 +101,52 @@ const createOne = () => {
 
 const createTwo = () => {
     const container = document.querySelector("#irigaray-content .shape-container");;
-    const geometry = new TorusKnotGeometry(2, 2, 300, 3, 2, 3);
+    // const geometry = new TorusKnotGeometry(2, 2, 300, 3, 2, 3);
+    const geometry = new TorusKnotGeometry(3, 1, 300, 20, 10, 15);
     const material = new MeshLambertMaterial({ color: 0xFEFBD8 });
+    const shape = new Mesh(geometry, material);
+    object(container, shape);
+};
+
+const createThree = () => {
+    const container = document.querySelector("#tripaldi-content .shape-container");;
+    const geometry = new TorusKnotGeometry(3.5, 0.5, 300, 20, 10, 12);
+    const material = new MeshLambertMaterial({ color: 0xEECEB9 });
     const shape = new Mesh(geometry, material);
     object(container, shape);
 };
 
 createOne();
 createTwo();
+createThree();
 
 const drawerOpener = () => {
     const links = document.querySelectorAll(".link");
     const drawers = document.querySelectorAll(".content-drawer");
-    const closeBtn = document.getElementById("close-btn");
-    const textBehind = document.querySelector(".introduction-section");
+    const closeBtns = document.querySelectorAll(".close-btn");
+    const mainContent = document.querySelector(".introduction-section");
+    const mainContentParagraphs = mainContent.querySelectorAll("p");
     links.forEach(link => {
         const linkId = link.id;
         drawers.forEach(drawer => {
             const drawerId = drawer.id;
             link.addEventListener("click", () => {
-                textBehind.classList.add("--opacity");
+                mainContent.classList.add("--opacity");
                 if (drawerId.includes(linkId)) {
                     drawer.classList.add("--open");
                 } else {
                     drawer.classList.remove("--open");
-                    textBehind.classList.remove("--opacity");
                 };
             });
         });
     });
-    closeBtn.addEventListener("click", () => {
-        drawers.forEach(drawer => {
-            drawer.classList.remove("--open");
+    closeBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            drawers.forEach(drawer => {
+                drawer.classList.remove("--open");
+            });
+            mainContent.classList.remove("--opacity");
         });
-        textBehind.classList.remove("--opacity");
     });
 };
 
